6819a2969caad25af39279a6c59ede53b13ed3d6
Praveenmail2him <Praveenmail2him@gmail.com>
2016-08-02 23:10:55 +0530

    Update OpenshiftKmApplication.java

--- src/main/java/com/infosys/km/openshiftdemo/OpenshiftKmApplication.java
+++ src/main/java/com/infosys/km/openshiftdemo/OpenshiftKmApplication.java
@@ -1,11 +1,16 @@
 package com.infosys.km.openshiftdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class OpenshiftKmApplication {
 
