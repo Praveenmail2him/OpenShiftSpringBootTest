d211fbbe114896347bd0477353aa9bea473fb4d2
Praveenmail2him <Praveenmail2him@gmail.com>
2016-08-02 12:43:24 +0530

    Add files via upload

---
+++ OpenshiftKM/src/main/java/com/infosys/km/openshiftdemo/OpenshiftKmApplication.java
@@ -0,0 +1,12 @@
+package com.infosys.km.openshiftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftKmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftKmApplication.class, args);
+	}
+}
